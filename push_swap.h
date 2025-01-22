c3d7f4018b211cb96031fb317116ba6b5383a939
ylabser <144272517+besrofex@users.noreply.github.com>
2025-01-22 21:05:25 +0100

    Update push_swap.h

--- push_swap.h
+++ push_swap.h
@@ -10,3 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+// stack management
+int	ft_atoi(const char *str);
+
+
+// main
+char	**ft_split(char const *s, char c);
+
+
+#endif
